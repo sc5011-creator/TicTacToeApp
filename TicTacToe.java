db78b7431eaad19b159dec6cdfc15f997399d946
Shailja <sc5011@gmail.com>
2026-05-02 20:13:17 +0530

    UC7: Computer random move implemented

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,17 +1,62 @@
+import java.util.Random;
+
 public class TicTacToe {
 
-    static char[][] board = new char[3][3];
+    static char[][] board = {
+        {'-', '-', '-'},
+        {'-', '-', '-'},
+        {'-', '-', '-'}
+    };
+
+    static char computerSymbol = 'O';
 
     public static void main(String[] args) {
+        computerMove();
 
-        // Sample move
-        placeMove(0, 0, 'X');
-
-        // Check output
-        System.out.println(board[0][0]);
+        // Print board after move (just to see output)
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 
-    // METHOD YOU NEED TO COMPLETE
+    // UC7 METHOD
+    static void computerMove() {
+        Random rand = new Random();
+
+        while (true) {
+            int slot = rand.nextInt(9) + 1; // 1–9
+
+            int row = getRowFromSlot(slot);
+            int col = getColFromSlot(slot);
+
+            if (isValidMove(row, col)) {
+                placeMove(row, col, computerSymbol);
+                break;
+            }
+        }
+    }
+
+    // UC4 reuse
+    static int getRowFromSlot(int slot) {
+        return (slot - 1) / 3;
+    }
+
+    // UC4 reuse
+    static int getColFromSlot(int slot) {
+        return (slot - 1) % 3;
+    }
+
+    // UC5 reuse
+    static boolean isValidMove(int row, int col) {
+        return row >= 0 && row < 3 &&
+               col >= 0 && col < 3 &&
+               board[row][col] == '-';
+    }
+
+    // UC6 reuse
     static void placeMove(int row, int col, char symbol) {
         board[row][col] = symbol;
     }
